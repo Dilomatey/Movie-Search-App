276db8160c7d907d613a94a41b394ddb91eeb667
Samuel Kudadji <sakud1@morgan.edu>
2019-09-30 10:19:28 -0500

    Initial Commit

---
+++ Movie Search App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Search App
+//
+//  Created by Samuel Kudadji on 9/30/19.
+//  Copyright © 2019 Samuel Kudadji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
